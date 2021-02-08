1f7ce29d92de2a0d5571b30e37d83eeb58d45991
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-08 09:34:33 +0200

    add lesson1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const stars = 55;
-let price;
-if (stars === 1) {
-    price = 20;
-}
-else if (stars === 2) {
-    price = 48
-}
-else {
-    console.log("no no");
-}
-console.log(price);
+// const stars = 55;
+// let price;
+// if (stars === 1) {
+//     price = 20;
+// }
+// else if (stars === 2) {
+//     price = 48
+// }
+// else {
+//     console.log("no no");
+// }
+// console.log(price);
