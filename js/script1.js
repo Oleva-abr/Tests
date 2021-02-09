0be841dbee288cf89dc64c738b8c251e1ec8b574
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-09 08:35:19 +0200

    hw2

---
+++ js/script1.js
@@ -0,0 +1,22 @@
+// const min = 3;
+// const max = 6;
+// let total = 0;
+// for (let i = min; i <= max; i+=1){
+//     // console.log(i);
+//     if (i % 2 !== 0) {
+//         console.log("не четное", i);  
+//         continue;  
+//     }
+//     console.log("четное", i);
+//     total += i;
+// }
+// console.log("total:", total);
+
+
+
+// let balance = 500;
+// const payment = 2000;
+// console.log(`Если стоимость заказа ${2000}khjhjkhjkhljh`);
+// if (balance <= payment) {
+//     console.log('ok');
+//  }
---
+++ js/script4.js
@@ -0,0 +1,212 @@
+// Задача 2
+
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   // Пиши код ниже этой строки
+//   let message;
+
+//   if (password === ADMIN_PASSWORD) {
+//     message = 'Добро пожаловать!';
+//   } else {
+//     message = 'Доступ запрещен, неверный пароль!';
+//   }
+
+//   return message;
+//   // Пиши код выше этой строки
+// }
+// checkPassword('mangohackzor');
+// checkPassword('polyhax');
+// checkPassword('jqueryismyjam');
+
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   // Пиши код ниже этой строки
+ 
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Добро пожаловать!';
+//   } 
+   
+//   return 'Доступ запрещен, неверный пароль!';
+//   // Пиши код выше этой строки
+// }
+// checkPassword('mangohackzor');
+// checkPassword('polyhax');
+// checkPassword('jqueryismyjam');
+
+
+
+// Задача 3
+// function checkStorage(available, ordered) {
+//   // Пиши код ниже этой строки
+//   let message;
+
+//   if (ordered === 0) {
+//     message = 'В заказе еще нет товаров';
+//   } else if (ordered > available) {
+//     message = 'Слишком большой заказ, на складе недостаточно товаров!';
+//   } else {
+//     message = 'Заказ оформлен, с вами свяжется менеджер';
+//   }
+
+//   return message;
+//   // Пиши код выше этой строки
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(70, 0);
+// checkStorage(200, 20);
+// checkStorage(200, 250);
+// checkStorage(150, 0);
+
+
+// function checkStorage(available, ordered) {
+//   // Пиши код ниже этой строки
+ 
+
+//   if (ordered === 0) {
+//     return 'В заказе еще нет товаров';
+//   } 
+//  if (ordered > available) {
+//     return 'Слишком большой заказ, на складе недостаточно товаров!';
+ 
+//   }
+//    return 'Заказ оформлен, с вами свяжется менеджер';
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(70, 0);
+// checkStorage(200, 20);
+// checkStorage(200, 250);
+// checkStorage(150, 0);
+
+
+// Задача 4
+// // Пиши код ниже этой строки
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+
+// Задача 5
+// Стартовый код
+// const fruits = ['яблоко','слива','груша','апельсин'];
+// // Пиши код ниже этой строки
+//  const firstElement =fruits[0];
+// const secondElement =fruits[1];
+// const lastElement = fruits[3];
+
+// Задача 6
+// // Стартовый код
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+// // Пиши код ниже этой строки
+//  fruits [1] = 'персик';
+// fruits [3] = 'банан';
+
+// Задача 7
+// // Стартовый код
+// const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// // Пиши код ниже этой строки
+// const fruitsArrayLength = fruits.length
+
+// Задача 8
+// // Стартовый код
+// const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// // Пиши код ниже этой строки
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex]
+
+
+
+// Задача 9
+// Напиши функцию getExtremeElements(array) которая принимает один параметр array
+//   - массив элементов произвольной длины.
+// Функция должна возвращать массив из двух элементов -
+//   первого и последнего элемента параметра array.
+// function getExtremeElements(array) {
+//     // Пиши код ниже этой строки
+//     const deleteArray =array.splice(1, array.length -2);
+// return array
+
+
+//     // Пиши код выше этой строки
+//   }
+// getExtremeElements([1, 2, 3, 4, 5]);
+// getExtremeElements(['Земля', 'Марс', 'Венера']);
+// getExtremeElements(['яблоко', 'персик', 'груша', 'банан']);
+
+
+// Задача 10
+// Дополни код функции splitMessage(message, delimeter) так, чтобы она возвращала
+// в переменной words результат разделения строки message по разделителю delimeter - массив строк.
+// function splitMessage(message, delimeter) {
+//   let words;
+//   // Пиши код ниже этой строки
+  
+//   words = message.split(delimeter)
+  
+//   // Пиши код выше этой строки
+//   return words;
+// }
+// splitMessage('Манго спешит на поезд', ' ');
+// splitMessage('Манго', '');
+// splitMessage('лучшее_за_неделю', '_');
+
+
+
+// Задача про высокосный год
+// Високосный год.Просто и без изысков.
+// function leapYear(year) {
+//   if (year % 4 === 0 && year % 100 !== 0) {
+//     console.log(`${year}-й год високосный`)
+//   } else if (year % 400 === 0) {
+//     console.log(`${year}-й год високосный`)
+//   } else {
+//     console.log(`${year}-й год невисокосный`)
+//   }
+// }
+// leapYear(2000) // високосный
+// leapYear(1900) // не високосный
+// leapYear(1904) // високосный
+// leapYear(2020) // високосный
+// leapYear(2021) // не високосный
+// leapYear(1800) // не високосный
+
+// Задача 13
+// function slugify(title) {
+//   // Пиши код ниже этой строки
+ 
+// const array = title.toLowerCase();
+//    const word = array.split(' ');
+//   const slug = word.join('-')
+//   // Пиши код выше этой строки
+//   return slug
+// }
+// slugify('Массивы для новичков');
+// slugify('Английский для разработчика');
+// slugify('Десять секретов JavaScript');
+// slugify('Как стать JUNIOR разработчиком за ДВЕ НЕДЕЛИ');
+
+// Задача 14
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин', 'банан'];
+// // Пиши код ниже этой строки
+// const firstTwoEls = fruits.slice(0,2);
+// const nonExtremeEls = fruits.slice(1,4);
+// const lastThreeEls = fruits.slice(-3);
+
+
+// // Задача 15
+// const oldClients = ['Манго', 'Аякс', 'Поли', 'Киви'];
+// const newClients = ['Персик', 'Хьюстон'];
+
+// const allClients = oldClients.concat(newClients); // Дополни эту строку
+
+// Задача 16
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Пиши код ниже этой строки
+
+//   const array = firstArray.concat(secondArray);
+//   const newArray = array.slice(0, maxLength)
+//   return newArray 
+
+    
+//     // Пиши код выше этой строки
+//   }
+
