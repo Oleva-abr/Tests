67ecb981864127deb1f76a63c1681659b3fde981
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-10 21:58:24 +0200

    +

--- js/script4.js
+++ js/script4.js
@@ -573,3 +573,47 @@
 // for ( const key in apartment) {
 //   keys.push(key);
 // values.push(apartment[key]);}
+
+
+// Задача 11
+// Выполни рефакторинг
+// решения предыдущего задания добавив в цикл
+// for...in проверку на собственное свойство.
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Просторная квартира в центре';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Пиши код ниже этой строки
+
+//   keys.push(key);
+//   values.push(apartment[key]);
+
+//   // Пиши код выше этой строки
+// }
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Просторная квартира в центре';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Пиши код ниже этой строки
+
+  
+// if ( apartment.hasOwnProperty(key)){
+// keys.push(key);
+//   values.push(apartment[key]);}
+//   // Пиши код выше этой строки
+// }
