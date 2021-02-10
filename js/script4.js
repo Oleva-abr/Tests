d13645440d9e047ca8e7eadde50541c3fecc0e0a
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-10 11:34:17 +0200

    1

--- js/script4.js
+++ js/script4.js
@@ -251,9 +251,160 @@
 // function createArrayOfNumbers(min, max) {
 //   const numbers = [];
 //   // Пиши код ниже этой строки
-// for (var i = min; i <= max; i++) {
+// for (var i = min; i <= max; i=+1) {
 //     numbers.push(i);
 // }
 //   // Пиши код выше этой строки
 //   return numbers;
+// }
+
+// Задача 23
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+// let NewArray =[];
+// for(let i=0; i> value; i +=1){
+
+//  NewArray = numbers.push(i);
+// }
+
+//   // Пиши код выше этой строки
+// return NewArray;
+// } - Не рабочая версия
+
+// Рабочая версия 23 
+// function filterArray(numbers, value) {
+//  let filteredNumbers = [];
+// for(const number of numbers) {
+//  if(number > value) {
+//    filteredNumbers.push(number);
+//  }
+// }
+// return filteredNumbers;
+// } (edited) 
+
+// Задача 24
+// function checkFruit(fruit) {
+//   const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+
+//   return fruits.includes(fruit); // Дополни эту строку
+// }
+ 
+
+// Задача 25
+
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+
+// Например, в двух массивах [1, 3, 5] и [0, 8, 5, 3] общими будут числа 3 и 5, т.к. они присутствуют в обоих исходных массивах. А числа 0, 1 и 8 присутствуют только в одном из массивов.
+
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2, и возвращает новый массив, состоящий из тех
+// элементов, которые присутствуют в обоих исходных массивах.
+
+// function getCommonElements(array1, array2) {
+//   // Пиши код ниже этой строки
+// let newArray =[];
+//   for(let i=0; i< array1.length; i+=1 ){
+//     if (array2.includes(array1[i])){
+//     newArray.push(array1[i])}
+//   }
+// return newArray;
+  
+//   // Пиши код выше этой строки
+// }
+
+
+// Задача 26
+// Выполни рефакторинг кода функции
+// calculateTotalPrice(order) заменив цикл for на for...of.
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Пиши код ниже этой строки
+
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+
+//   // Пиши код выше этой строки
+//   return total;
+// }
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Пиши код ниже этой строки
+
+//   for (const element of order) {
+//     total += element;
+//   }
+
+//   // Пиши код выше этой строки
+//   return total;
+// }
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+//   const filteredNumbers = [];
+
+//   for (const element of numbers) {
+//     const number = element;
+
+//     if (number > value) {
+//       filteredNumbers.push(element);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Пиши код выше этой строки
+// }
+
+
+
+
+// Задача 27
+// Выполни рефакторинг функции
+// filterArray(numbers, value) заменив цикл for на for...of.
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+//   const filteredNumbers = [];
+
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     const number = numbers[i];
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Пиши код выше этой строки
+// }
+
+// Задача 28
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3; 
+
+// Задача 29
+// function getEvenNumbers(start, end) {
+//     // Пиши код ниже этой строки
+// let array = [];
+//   for(i=start; i<= end; i+=1){
+//     if (i%2 ===0){
+//   i+=array[i];}}
+//   return array;
+//     // Пиши код выше этой строки
+  
+//   }
+
+// Задача 32
+// function includes(array, value) {
+//   // Пиши код ниже этой строки
+
+//   for (let i= 0; i<= array.length; i+=1){
+//   if (value === array[i]){return true;}
+//   }
+  
+// return false;
+  
+//   // Пиши код выше этой строки
 // }
