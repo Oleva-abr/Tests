8e993a742a41aaa6924ceaf654724c158a425bd7
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-10 18:20:48 +0200

    added

--- js/script4.js
+++ js/script4.js
@@ -408,3 +408,168 @@
   
 //   // Пиши код выше этой строки
 // }
+
+
+// Module 3
+// Задача1
+// const apartment = {
+// imgUrl:'https://via.placeholder.com/640x480',
+// descr:'Просторная квартира в центре',
+// rating:4,
+// price: 2153,
+// tags:['premium', 'promoted', 'top'],};
+
+// // Задача2
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+  
+//     owner:{
+//         name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//     },}  ;
+
+
+
+// // Задача3
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//   };
+  
+//   // Пиши код ниже этой строки
+//   const aptRating = apartment.rating;
+//   const aptDescr = apartment.descr;
+//   const aptPrice = apartment.price;
+//   const aptTags = apartment.tags;
+//   // Пиши код выше этой строки
+
+// Задача 4
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Пиши код ниже этой строки
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2];
+// // Пиши код выше этой строки
+
+
+// // Задача 5
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// // Пиши код ниже этой строки
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// // Пиши код выше этой строки
+
+
+//  Задача 6
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com'
+//   }
+// };
+
+// // Пиши код ниже этой строки
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Генри Сибола';
+// apartment.tags.push('trusted');
+
+// Задача 7
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Генри Сибола',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com'
+//   }
+// };
+
+// // Пиши код ниже этой строки
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location ={country:'Ямайка',city: 'Кингстон'};
+
+
+// Задача 8
+// const name = 'Ремонтный дроид';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   // Пиши код ниже этой строки
+//   name,
+//   price,
+// image,
+//   tags
+
+//   // Пиши код выше этой строки
+// };
+
+// Задача 9
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Пиши код ниже этой строки
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//    [passwordInputName]: 'jqueryismyjam'
+  
+  
+//   // Пиши код выше этой строки
+// };
+
+// Задача 10
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Пиши код ниже этой строки
+// for ( const key in apartment) {
+//   keys.push(key);
+// values.push(apartment[key]);}
