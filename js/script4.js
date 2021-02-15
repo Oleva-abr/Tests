ddf4ac8ea3326f1a5a815b67671ca4c0d49eaa1c
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-15 18:22:33 +0200

    +

--- js/script4.js
+++ js/script4.js
@@ -1286,3 +1286,71 @@
 //     // Пиши код выше этой строки
 //   },
 // };
+
+// Задача 40
+// Дополни метод updatePotionName(oldName, newName) так, чтобы он обно
+// название зелья с oldName на newName, в массиве зелий в свойстве potions.
+// const atTheOldToad = {
+//   potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+//   updatePotionName(oldName, newName) {
+//     // Пиши код ниже этой строки
+//  const potionsIndex = this.potions.indexOf(oldName);
+//     this.potions.splice(potionsIndex,1,newName);
+//     // Пиши код выше этой строки
+//   },
+// };
+
+
+// Задача 41
+// Заказчица хочет чтобы каждое зелье было представлено не
+// только именем, но и ценой, а в будущем может быть и другими
+// характеристиками.Поэтому теперь в свойстве
+// potions будет храниться массив объектов со следующими
+// свойствами.
+// Выполни рефакторинг методов объекта atTheOldToad так,
+//   чтобы они работали не с массивом строк, а с массивом объектов.
+
+//   getPotions() - метод для получения всех зелий.
+// Возвращает значение свойства potions.
+//   addPotion(newPotion) - добавляет зелье
+// newPotion(уже объект) в массив в свойстве potions.
+//   removePotion(potionName) - удаляет объект зелья с именем
+// potionName из массива в свойстве potions.
+//   updatePotionName(oldName, newName) - обновляет свойство
+// name объекта - зелья с названием oldName на newName в массиве potions.
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Зелье скорости', price: 460 },
+//     { name: 'Дыхание дракона', price: 780 },
+//     { name: 'Каменная кожа', price: 520 },
+//   ],
+//   // Пиши код ниже этой строки
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     if (this.potions.includes(newPotion)) {
+//       return `Зелье ${newPotion} уже есть в инвентаре!`;
+//     }
+
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(newPotion) {
+   
+// 	for(let i=0; i<this.potions.length; i+=1){
+//      if(this.potions[i].name === newPotion){this.potions.splice(i, 1);} }
+  
+      
+//     },
+  
+//   updatePotionName(oldName, newName) {
+    
+//     for(let i=0; i<this.potions.length; i+=1){ if(this.potions[i].name === oldName){
+//       this.potions[i].name = newName;
+//     }}
+ 
+//   },
+//   // Пиши код выше этой строки
+// };
+
