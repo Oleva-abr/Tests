a14d7e59ac051f993c8e5480a3c955db6d28c9d9
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-17 18:20:03 +0200

    +

--- js/script5.js
+++ js/script5.js
@@ -69,14 +69,14 @@
 //   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
 //   order(pizzaName, onSuccess, onError) {
 //         const error =  `В ассортименте нет пиццы с названием ${pizzaName}.`
-       
+
 //     if (this.pizzas.includes(pizzaName))
 //     { return onSuccess(pizzaName);;}
-    
+
 //  	return onError(error);
 
 //   },
-  
+
 // };
 // // Пиши код выше этой строки
 
@@ -352,3 +352,267 @@
 // child.age = 27;
 
 // Пиши код выше этой строки
+
+// Задача 3
+// Объяви функцию-конструктор Car которая принимает три параметра:
+
+// brand - марка автомобиля.
+// model - модель автомобиля.
+// price - цена автомобиля.
+// Функция Car должна создавать объект с одноимёнными
+// свойствами brand, model и price, значениями которых должны быть
+// переданные аргументы во время её вызова с оператором new.
+
+// function Car (brand, model, price) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// Задача 4
+// Выполни рефакторинг функции - конструктора Car
+// так, чтобы она принимала один параметр - объект со свойсвами brand,
+//     model и price.Деструктуризируй объект в сигнатуре(подписи) функции.
+// function Car({brand, model, price}) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+
+// Задача 5
+// Добавь в свойство prototype функции-конструктора Car два метода:
+
+// getPrice() - возвращает значение свойства price из объекта который
+// его будет вызывать.
+//     changePrice(newPrice) - обновляет значение свойства price
+// у объекта который его будет
+// вызывать на newPrice.
+
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+
+// Car.prototype.getPrice = function () {
+//   return this.price;
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+// };
+
+
+// Задача 6
+// Напиши функцию - конструктор Storage, которая будет создавать
+// объекты для управления складом товаров.Функция ожидает только
+// один аргумент - начальный массив товаров, который записывается на
+// создаваемый объект в свойство items.
+
+// Добавь методы на прототип:
+
+// getItems() - возвращает массив текущих товаров в свойстве items объекта,
+//     который вызывает этот метод.
+//     addItem(newItem) - принимает новый товар newItem и добавляет его
+// в массив товаров в свойстве items объекта, который вызывает этот метод.
+//     removeItem(item) -function Storage({ items }) {
+
+
+// function Storage(items) {
+//   this.items = items;
+// }
+// Storage.prototype.getItems=function(){
+
+//   return this.items;
+
+// };
+// Storage.prototype.addItem=function(newItem){
+// this.items.push(newItem);
+// };
+// Storage.prototype.removeItem=function(item){
+//   	const indexItem = this.items.indexOf(item);
+
+//  	this.items.splice(indexItem,1);
+ 
+// };
+
+
+
+// // Пиши код выше этой строки
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"] принимает товар item и удаляет его из массива товаров
+// // в свойстве items объекта, который вызывает этот метод.
+// // Под комментарием мы добавили инициализацию экземпляра и вызовы методов
+// // в тойпоследовательности, в которой твой код будут проверять тесты.
+// // Пожалуйста ничего там не меняй.
+
+
+
+
+// Задача 7
+// Напиши функцию - конструктор StringBuilder,
+//     которая принимает один параметр baseValue - произвольную строку,
+//         которая записывается на создаваемый объект в свойство value.
+
+// Добавь методы на прототип:
+
+// getValue() - возвращает текущее значение свойства value.
+//     padEnd(str) - получает парметр str(строку) и добавляет её в
+// конец значения свойства value объекта, который вызывает этот метод.
+//     padStart(str) - получает парметр str(строку) и добавляет её в начало
+// значения свойства value объекта, который вызывает этот метод.
+//     padBoth(str) - получает парметр str(строку) и добавляет её в начало
+// и в конец значения свойства value объекта, который вызывает этот метод.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы методов в
+// той последовательности, в которой твой код будут проверять тесты.
+// Пожалуйста ничего там не меняй.
+
+
+
+// function StringBuilder(baseValue){
+// this.value = baseValue;
+// }
+
+// StringBuilder.prototype.getValue =function(){
+// return this.value;
+// };
+
+// StringBuilder.prototype.padEnd =function(str){
+// this.value +=str;
+// };
+
+
+// StringBuilder.prototype.padStart=function(str){
+  
+// this.value =str + this.value;
+// };
+
+// StringBuilder.prototype.padBoth=function(str){
+// this.value = str + this.value +str;
+// };
+
+
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+
+// Задача 8
+// Используя ключевое слово class объяви класс Car с пустым телом.
+
+// class Car{
+  
+// }
+
+// Задача 9
+// Выполни рефакторинг кода, заменив функцию -
+//     конструктор Car на класс с методом - конструктором, принимающим объект.
+
+// class Car{
+//   brand;
+//   model;
+//   price;
+//   constructor({ brand, model,price }) {
+//      this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// Задача 10
+// Добавь классу Car две метода.
+
+//     getPrice() - возвращает значение свойства price из объекта который его
+// будет вызывать.
+//     changePrice(newPrice) - обновляет значение свойства price у объекта
+// который
+// его будет вызывать на newPrice.
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+// getPrice() {
+// return this.price;
+// }
+// changePrice(newPrice){
+//   this.price = newPrice;
+//     }
+// }
+
+
+// Задача 11
+// Выполни рефакторинг класса Car так, чтобы свойство brand
+// было приватным и добавь два метода для публичного интерфейса,
+//     для чтения и изменения этого свойства.
+
+// getBrand() - возвращает значение приватного свойства brand.
+//     changeBrand(newBrand) - изменяет значение приватного свойства
+// brand на newBrand.
+
+// class Car {
+//   #brand; 
+    
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand(){
+//   return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//   this.#brand = newBrand;
+//   }
+// }
+
+
+// Задача 12
+// Выполни рефакторинг заменив функцию - конструктор Storage
+// на класс с методами.Сделай так, чтобы свойство items было приватным.
+
+// Под комментарием мы добавили инициализацию экземпляра и вызовы
+// методов в той последовательности, в которой твой код будут проверять
+// тесты.Пожалуйста ничего там не меняй.
+
+// class Storage { #items;
+//   constructor( items) {             
+//   this.#items = items;
+// }
+
+// getItems() {
+//   return this.#items;
+// }
+
+// addItem(newItem) {
+//   this.#items.push(newItem);
+// }
+
+// removeItem(item) {
+//   const itemIndex = this.#items.indexOf(item);
+//   this.#items.splice(itemIndex, 1);
+// }
+// }
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
