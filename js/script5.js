9f54c1c1233e3e7eea557bf7fec95f0d1f32c905
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-21 21:48:19 +0200

    +

--- js/script5.js
+++ js/script5.js
@@ -815,7 +815,7 @@
 //     BASIC: 'basic',
 //     SUPERUSER: 'superuser' ,
 //   };
-  
+
 // }
 
 // Задача 18
@@ -846,7 +846,7 @@
 
 // class Admin extends User {
 //   // Пиши код ниже этой строки
-   
+
 //    accessLevel;
 //     constructor({ email, accessLevel }) {
 //     super(email);
@@ -855,8 +855,8 @@
 //   static AccessLevel = {
 //     BASIC: 'basic',
 //     SUPERUSER: 'superuser'
-    
-       
+
+
 //     };
 //   // Пиши код выше этой строки
 // }
@@ -910,7 +910,7 @@
 
 //   accessLevel;
 //   blacklistedEmails =[];
-  
+
 //   constructor({ email, accessLevel }) {
 //     super(email);
 //     this.accessLevel = accessLevel;
@@ -920,9 +920,9 @@
 //   this.blacklistedEmails.push(email);
 //   }
 //   isBlacklisted(email){
-    
+
 //   return this.blacklistedEmails.includes(email)
-  
+
 //   }
 //   // Пиши код выше этой строки
 // }
@@ -975,13 +975,13 @@
 // function filterArray(numbers, value) {
 //     const filteredNumbers = [];
 //     // Пиши код ниже этой строки
-  
+
 //     numbers.forEach (function(number) {
 //       if (number > value) {
 //         filteredNumbers.push(number);
 //       }
 //     });
-  
+
 //     // Пиши код выше этой строки
 //     return filteredNumbers;
 //   }
@@ -999,13 +999,13 @@
 // function getCommonElements(firstArray, secondArray) {
 //     const commonElements = [];
 //     // Пиши код ниже этой строки
-  
+
 //     firstArray.forEach (function (number) {
 //       if (secondArray.includes(number)) {
 //         commonElements.push(number);
 //       }
 //     });
-  
+
 //     return commonElements;
 //     // Пиши код выше этой строки
 //   }
@@ -1089,3 +1089,313 @@
 
 // Задача 5
 // Выполни рефакторинг функции calculateTotalPrice() так, чтобы она использовала неявный возврат.
+
+// // Пиши код ниже этой строки
+// const calculateTotalPrice = (quantity, pricePerItem) =>  quantity * pricePerItem;
+// // Пиши код выше этой строки
+
+
+// Задача 6
+// Выполни рефакторинг функции calculateTotalPrice(orderedItems) заменив её объявление на стрелочную функцию.
+// Замени коллбек - функцию передаваемую в метод forEach() на стрелочную функцию.
+
+// // Пиши код ниже этой строки
+// const calculateTotalPrice=(orderedItems)=> {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item)=> {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+// Пиши код выше этой строки
+
+
+// Задача 7
+// Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции.
+// Пиши код ниже этой строки
+// const filterArray=(numbers, value)=> {
+//     const filteredNumbers = [];
+
+//     numbers.forEach( (number)=> {
+//       if (number > value) {
+//         filteredNumbers.push(number);
+//       }
+//     });
+
+//     // Пиши код выше этой строки
+//     return filteredNumbers;
+//   }
+
+
+// Задача 8
+// Замени объявление функции getCommonElements() и коллбек для метода forEach() на стрелочные функции.
+// Пиши код ниже этой строки
+// const getCommonElements =(firstArray, secondArray)=> {
+//     const commonElements = [];
+  
+//     firstArray.forEach( (element)=> {
+//       if (secondArray.includes(element)) {
+//         commonElements.push(element);
+//       }
+//     });
+  
+//     // Пиши код выше этой строки
+//     return commonElements;
+//   }
+
+
+// Задача 9
+// Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент,
+//     значение которого это чётное число, добавляя к нему значение параметра value.
+
+// Выполни рефакторинг функции так, чтобы она стала чистой - не изменяла массив чисел numbers,
+//     а создавала, наполняла и возвращала новый массив с обновлёнными значениями.
+
+// function changeEven(numbers, value) {
+//     // Пиши код ниже этой строки
+//   const array = [];
+//   numbers.forEach (element =>{
+//       if (element % 2 === 0) {
+//        element += value;
+        
+//       }
+//     array.push(element);
+//     });
+//   return array;
+//     // Пиши код выше этой строки
+//   }
+
+
+// Задача 10
+// Дополни код так, чтобы в переменной planetsLengths
+// получился массив длин названий планет.Обязательно используй метод map().
+
+// const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+// // Пиши код ниже этой строки
+// const planetsLengths = planets.map((planet)=>planet.length);
+
+// Задача 11
+// Используя метод map() сделай так, чтобы в переменной titles получился массив названий
+// книг(свойство title) из всех объектов массива books.
+
+// const books = [
+//     { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//     { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//     { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//     { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//     { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+//   ];
+//   // Пиши код ниже этой строки
+  
+//   const titles = books.map(book=>book.title);
+
+
+// Задача 12
+// Используя метод flatMap() сделай так, чтобы в переменной genres получился массив всех жанров книг
+//   (свойство genres) из массива книг books.
+
+// const books = [
+//     {
+//       title: 'Последнее королевство',
+//       author: 'Бернард Корнуэлл',
+//       genres: ['приключения', 'историческое']
+//     },
+//     {
+//       title: 'На берегу спокойных вод',
+//       author: 'Роберт Шекли',
+//       genres: ['фантастика']
+//     },
+//     {
+//       title: 'Красна как кровь',
+//       author: 'Ли Танит',
+//       genres: ['ужасы', 'мистика']
+//     }
+//   ];
+//   // Пиши код ниже этой строки
+  
+//   const genres = books.flatMap((book)=>book.genres);
+
+// Задача 13
+// Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей
+//   (свойство name) из массива объектов в параметре users.
+
+// [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ['ex', 'culpa', 'nostrud'],
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ['non', 'amet', 'ipsum'],
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ['lorem', 'veniam', 'culpa'],
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
+// // Пиши код ниже этой строки
+// const getUserNames = users =>users.map(user =>  user.name);  
+  
+
+  
+//   // Пиши код выше этой строки
+
+
+
+// Задача 14
+
+// Дополни функцию getUserEmails(users) так, чтоб
+// ы она возвращала массив почтовых адресов пользователей(свойство email)
+// из массива объектов в параметре users.
+// // Пиши код ниже этой строки
+// const getUserEmails = users => users.map(user =>  user.email);  
+    
+
+  
+//   // Пиши код выше этой строки
+
+
+
+
+
+
+// Задача 15
+// Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers,
+//   а в переменной oddNumbers массив нечётных.Обязательно используй метод filter().
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Пиши код ниже этой строки
+
+// const evenNumbers = numbers.filter(value => value %2===0);;
+// const oddNumbers = numbers.filter(value => value%2===1 );;
+
+
+// Задача 16
+// Дополни код так, чтобы в переменной allGenres был массив всех жанров книг(свойство genres)
+// из массива books,
+//   а в переменной uniqueGenres массив уникальных жанров - без повторений.
+
+// const books = [
+//     {
+//       title: 'Последнее королевство',
+//       author: 'Бернард Корнуэлл',
+//       genres: ['приключения', 'историческое']
+//     },
+//     {
+//       title: 'На берегу спокойных вод',
+//       author: 'Роберт Шекли',
+//       genres: ['фантастика', 'мистика']
+//     },
+//     {
+//       title: 'Красна как кровь',
+//       author: 'Ли Танит',
+//       genres: ['ужасы', 'мистика', 'приключения']
+//     }
+//   ];
+//   // Пиши код ниже этой строки
+//   const allGenres = books.flatMap(book => book.genres);
+//   const uniqueGenres = allGenres.filter((genr,index,array)=>array.indexOf(genr) === index);
+
+
+// Задача 17
+// Используя метод filter() дополни код так, чтобы:
+
+// В переменной topRatedBooks получился массив книг рейтинг которых(свойство rating)
+// больше либо равно значению переменной MIN_RATING.
+// В переменной booksByAuthor получился массив книг написанных автором с именем(свойство author)
+// которое совпадает со значением
+// в переменной AUTHOR.
+
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//   { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = 'Бернард Корнуэлл';
+// // Пиши код ниже этой строки
+
+// const topRatedBooks = books.filter((book)=>book.rating>=MIN_RATING);
+// const booksByAuthor = books.filter((book)=>book.author===AUTHOR);
+
+
+
+
+// Задача 18
+// Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала массив пользователей
+// у которых цвет
+// глаз(свойство eyeColor) совпадает со значением параметра color.
+
+// Пиши код ниже этой строки
+
+const getUsersWithEyeColor = users.filter((user,color)=>user.eyeColor===color);
+// Пиши код выше этой строки
