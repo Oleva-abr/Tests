b1dccb1621b5b1bcbbc739068e030402694997a7
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-22 23:01:22 +0200

    +

--- js/script5.js
+++ js/script5.js
@@ -1736,17 +1736,47 @@
 // Дополни код так, чтобы в переменной names получился массив имён авторов в
 // алфавитном порядке, рейтинг книг которых больше значения переменной MIN_BOOK_RATING.
 
-const books = [
-  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
-  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
-  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
-  { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
-  { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 }
-];
-const MIN_BOOK_RATING = 8;
-// Пиши код ниже этой строки
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+//   { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 }
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Пиши код ниже этой строки
 
-const names = [...books]
-.flatMap((book) => book.author)
-.filter((book)=>book.rating>MIN_BOOK_RATING)
-.sort((a, b) => a.author.localeCompare(b.author));
+// const names = books
+
+
+// .filter((book)=>book.rating > MIN_BOOK_RATING)
+// .map((book) => book.author)
+// .sort((a, b) => a.localeCompare(b));
+
+
+// Задача 42
+// Дополни функцию getNamesSortedByFriendCount(users) так, чтобы она возвращала
+// массив имён пользователей отсортированный по возрастанию количества их друзей(свойство friends).
+
+// Пиши код ниже этой строки
+// const getNamesSortedByFriendCount = users =>
+   
+
+// [...users]
+// .sort((a,b)=>a.friends.length-b.friends.length)
+// .map((user) => user.name); 
+
+
+// Пиши код выше этой строки
+
+
+// Задача 43
+// Дополни функцию getSortedFriends(users) так, чтобы она
+// возвращала массив уникальных имён друзей(свойство friends) отсортированный по алфавиту.
+
+// Пиши код ниже этой строки
+// const getSortedFriends = users =>  
+// [...users].flatMap((user) => user.friends)
+//   .filter((friend, index, array) => array.indexOf(friend) === index)
+//   .sort((a, b) => a.localeCompare(b));
+// Пиши код выше этой строки
