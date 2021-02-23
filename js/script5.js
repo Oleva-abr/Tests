8fe6f7702584414eaded70386915870aa0f5abe6
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-23 13:46:13 +0200

    +

--- js/script5.js
+++ js/script5.js
@@ -1797,3 +1797,114 @@
 // .reduce((acc,balance)=>{return acc+balance},0); 
 
 // // Пиши код выше этой строки
+// -----------------------------------------------------------------------------------------------
+// сlass Wheel {
+//     constructor(radius) {
+//         this.radius = radius;
+//     }
+// }
+// ​
+// class Trailer {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+// }
+// ​
+// class Car {
+// ​
+//     wheels = [];
+//     #speed = 0;
+//     #isOn = false;
+// ​
+//     constructor(radius) {
+//         for (let i = 0; i < 4; i++)
+//             this.wheels.push(new Wheel(radius));
+//     }
+// ​
+//     get isOn() {
+//         return this.#isOn;
+//     }
+// ​
+//     set isOn(value) {
+//         if (typeof value === 'boolean')
+//             this.#isOn = value;
+//     }
+// ​
+//     get speed() {
+//         return this.#speed;
+//     }
+// ​
+//     set speed(speed) {
+//         if (speed < 0) return;
+// ​
+//         if (speed === 0 || (speed > 0 && this.isOn))
+//             this.#speed = speed;
+//     }
+// ​
+//     turnOn() {
+//         if (this.isOn) return;
+// ​
+//         this.isOn = true;
+//         this.setSpeed(0);
+//     }
+// ​
+//     turnOff() {
+//         if (!this.isOn) return;
+// ​
+//         this.isOn = false;
+//         this.setSpeed(0);
+//     }
+// }
+// ​
+// class Volvo extends Car {
+//     constructor() {
+//         super(250);
+//     }
+// ​
+//     connectTrailer(trailer) {
+//         this.trailer = trailer;
+//     }
+// }
+// ​
+// class BMW extends Car {
+//     constructor() {
+//         super(150);
+//     }
+// ​
+//     getSpeed() {
+//         console.log('this best bmw');
+//         return this.speed;
+//     }
+// ​
+// }
+// ​
+// class Wheelbarrow {
+//     constructor() {
+//         this.wheel = new Wheel(80);
+//     }
+// }
+// ​
+// let passengerVolvo = new Volvo();
+// ​
+// let wagonVolvo = new Volvo();
+// wagonVolvo.connectTrailer(new Trailer(500, 1500));
+// ​
+// console.log(wagonVolvo);
+// ​
+// let passengerBmv = new BMW();
+// ​
+// let wheelbarrow = new Wheelbarrow();
+// Collapse
+
+
+
+
+
+
+
+
+
+
+
+
